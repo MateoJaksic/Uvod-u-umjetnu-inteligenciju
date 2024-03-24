26a13e4a0b21356b4ba2ed6beb4ad684de8ba704
MateoJaksic <mateo.jaksic@outlook.com>
2024-03-25 00:53:57 +0100

    Implementiran algoritam astar

--- autograder/data/lab1/templates/lab1py/solution.py
+++ autograder/data/lab1/templates/lab1py/solution.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+import heapq
 
 
 # Klasa za definiranje čvora
@@ -6,6 +8,10 @@ class Node:
     def __init__(self, name, parent):
         self.name = name
         self.parent = parent
+
+    # Implementacija lees than metode koju koristi heap queue
+    def __lt__(self, other):
+        return self.name < other.name
 
 
 
@@ -39,9 +45,9 @@ def get_heuristics_data(file):
         data = f.readlines()
 
     heuristics = {}
-    for i in range(len(data)):
-        if data[i][0] != "#":
-            heuristics.update({ key: int(value) for (key, value) in [data[i].rstrip("\n").split(": ")] })
+    for line in data:
+        if line[0] != "#":
+            heuristics.update({ key: int(value) for (key, value) in [line.rstrip("\n").split(": ")] })
     return heuristics
 
 
@@ -50,8 +56,8 @@ def get_heuristics_data(file):
 def bfs(start_state, end_state, successors):
     start_state = Node(start_state, None)  # Čvor u sebi ima informaciju o trenutnom stanju i njegovom parentu
     end_state = [state for state in end_state]  # Lista koja sadrži jedno ili više konačnih stanja
-    open = []  # Open je lista otvorenih stanja koja se sastoji od čvorova
-    closed = []  # Closed je lista zatvorenih stanja koja se sastoji od naziva stanja, ako bi ju punili čvorovima, onda bi razlikovalo ista stanja kao više različitih zbog parenta
+    open = deque()  # Open je lista otvorenih stanja koja se sastoji od čvorova i implementirana je kao deque kako bi smanjili vremensku složenost liste s O(n) prilikom pop() na O(1)
+    closed = set()  # Closed je lista zatvorenih stanja koja se sastoji od naziva stanja, ako bi ju punili čvorovima, onda bi razlikovalo ista stanja kao više različitih zbog parenta, implementirana je kao set() zbog prednosti u brzini nad listom
     total_cost = 0 
         
     # Dodajemo početno stanje u listu otvorenih
@@ -59,29 +65,29 @@ def bfs(start_state, end_state, successors):
     # Dok je lista otvorenih neprazna, prolazimo kroz nju
     while len(open) != 0:
         # Uzimamo prvo stanje iz liste otvorenih koje postaje trenutno stanje
-        current_state = open.pop(0)
-        # Ako taj čvor nije u listi zatvorenih, dodajemo ga 
+        current_state = open.popleft()
+        # Ako taj čvor nije u listi zatvorenih, dodajemo ga i obrađujemo, inaće zbog brzine preskačemo 
         if current_state.name not in closed:
-            closed.append(current_state.name)
+            closed.add(current_state.name)
+                
+            # Provjeravamo je li trenutno stanje konačno stanje
+            if current_state.name in end_state:
+                # Stvaramo putanju iz konačnog stanja do početnog stanja
+                path = []
+                while current_state.name != start_state.name:
+                    path.append(current_state.name)
+                    temp = current_state.name  # Spremamo ime trenutnog stanja
+                    current_state = current_state.parent  # Dohvaćamo parenta trenutnog stanja
+                    total_cost += successors[current_state.name][temp]  # Zbrajamo trošak putavanja iz parenta u trenutno stanje
+                path.append(start_state.name)
+
+                # Vraćamo potvrdu uspješnosti, broj prođenih stanja, duljinu putanje, totalnu cijenu putanje te putanju 
+                return "yes", len(closed), len(path), float(total_cost), " => ".join(list(reversed(path)))
+
+            # Ako trenutno stanje nije konačno stanje, ekspandiramo njegovu djecu i dodajemo u listu otvorenih po abecednom redoslijedu 
+            for state in sorted(successors[current_state.name].keys()):
+                open.append(Node(state, current_state))
             
-        # Provjeravamo je li trenutno stanje konačno stanje
-        if current_state.name in end_state:
-            # Stvaramo putanju iz konačnog stanja do početnog stanja
-            path = []
-            while current_state.name != start_state.name:
-                path.append(current_state.name)
-                temp = current_state.name  # Spremamo ime trenutnog stanja
-                current_state = current_state.parent  # Dohvaćamo parenta trenutnog stanja
-                total_cost += successors[current_state.name][temp]  # Zbrajamo trošak putavanja iz parenta u trenutno stanje
-            path.append(start_state.name)
-
-            # Vraćamo potvrdu uspješnosti, broj prođenih stanja, duljinu putanje, totalnu cijenu putanje te putanju 
-            return "yes", len(closed), len(path), float(total_cost), " => ".join(list(reversed(path)))
-
-        # Ako trenutno stanje nije konačno stanje, ekspandiramo njegovu djecu i dodajemo u listu otvorenih po abecednom redoslijedu 
-        for state in sorted(successors[current_state.name].keys()):
-            open.append(Node(state, current_state))
-
     # Ukoliko je pretraživanje bilo neuspješno vraćamo potvrdu neuspješnosti
     return "no", None, None, None, None
 
@@ -91,38 +97,82 @@ def bfs(start_state, end_state, successors):
 def ucs(start_state, end_state, successors):
     start_state = Node(start_state, None)
     end_state = [state for state in end_state]
-    open = []  # Open je lista otvorenih stanja koja se sastoji od dictionarya, koji sadrži vrijednosti čvora i cijene puta, po kojoj onda sortiramo listu
-    closed = []
+    open = []  # Open je lista otvorenih stanja, implementirana je kao heap queue zbog njegovih sposobnosti ubrzati vremensku složenost jer vraća uvijek najmanji element, a taj element se određuje prema prvom parametru tuplea koji odgovara cijeni prijalaza između stanja, dok drugi parametar tuplea odgovara čvoru stanja
+    heapq.heapify(open)  # Funkcija koja pretvara listu u heap queue
+    closed = set()  # Closed je lista zatvorenih stanja koja se sastoji od naziva stanja, ako bi ju punili čvorovima, onda bi razlikovalo ista stanja kao više različitih zbog parenta, implementirana je kao set() zbog brzinske prednosti nad listom
     total_cost = 0
         
     # Dodajemo početno stanje u listu otvorenih
-    open.append({"node": start_state, "price": 0})
+    heapq.heappush(open, (0, start_state))
     # Dok je lista otvorenih neprazna, prolazimo kroz nju
     while len(open) != 0:
         # Uzimamo prvo stanje iz liste otvorenih koje postaje trenutno stanje
-        current_state = open.pop(0)
-        # Ako taj čvor nije u listi zatvorenih, dodajemo ga 
-        if current_state["node"].name not in closed:
-            closed.append(current_state["node"].name)
+        price, current_state = heapq.heappop(open)
+        # Ako taj čvor nije u listi zatvorenih, dodajemo ga i obrađujemo, inaće zbog brzine preskačemo 
+        if current_state.name not in closed:
+            closed.add(current_state.name)
             
-        # Provjeravamo je li trenutno stanje konačno stanje
-        if current_state["node"].name in end_state:
-            path = []
-            while current_state["node"].name != start_state.name:
-                path.append(current_state["node"].name)
-                temp = current_state["node"].name  # Spremamo ime trenutnog stanja
-                current_state = {"node": current_state["node"].parent}  # Dohvaćamo parenta trenutnog stanja
-                total_cost += successors[current_state["node"].name][temp]  # Zbrajamo trošak putavanja iz parenta u trenutno stanje
-            path.append(start_state.name)
+            # Provjeravamo je li trenutno stanje konačno stanje
+            if current_state.name in end_state:
+                path = []
+                while current_state.name != start_state.name:
+                    path.append(current_state.name)
+                    temp = current_state.name  # Spremamo ime trenutnog stanja
+                    current_state = current_state.parent  # Dohvaćamo parenta trenutnog stanja
+                    total_cost += successors[current_state.name][temp]  # Zbrajamo trošak putavanja iz parenta u trenutno stanje
+                path.append(start_state.name)
 
-            # Vraćamo potvrdu uspješnosti, broj prođenih stanja, duljinu putanje, totalnu cijenu putanje te putanju 
-            return "yes", len(closed), len(path), float(total_cost), " => ".join(list(reversed(path)))
+                # Vraćamo potvrdu uspješnosti, broj prođenih stanja, duljinu putanje, totalnu cijenu putanje te putanju 
+                return "yes", len(closed), len(path), float(total_cost), " => ".join(list(reversed(path)))
 
-        # Ako trenutno stanje nije konačno stanje, ekspandiramo njegovu djecu i dodajemo u listu otvorenih  
-        for state in successors[current_state["node"].name].keys():
-            open.append({"node": Node(state, current_state["node"]), "price": current_state["price"] + (successors[current_state["node"].name])[state]})
-            # Sortiramo listu otvorenih po cijeni, u slučaju više stanja s istom cijenom sortiramo ih abecedno
-            open = sorted(open, key=lambda x: (x["price"], x["node"].name))
+            # Ako trenutno stanje nije konačno stanje, ekspandiramo njegovu djecu i dodajemo u listu otvorenih  
+            for state in successors[current_state.name].keys():
+                # Funkcijom dodajemo element u heap queue
+                heapq.heappush(open, (price + (successors[current_state.name])[state], Node(state, current_state)))
+
+    # Ukoliko je pretraživanje bilo neuspješno vraćamo potvrdu neuspješnosti
+    return "no", None, None, None, None
+
+
+def astar(start_state, end_state, successors, heuristics):
+    start_state = Node(start_state, None)
+    end_state = [state for state in end_state]
+    open = []  # Open je lista otvorenih stanja, implementirana je kao heap queue zbog njegovih sposobnosti ubrzati vremensku složenost jer vraća uvijek najmanji element, a taj element se određuje prema prvom parametru tuplea koji odgovara cijeni prijalaza između stanja, dok drugi parametar tuplea odgovara čvoru stanja
+    heapq.heapify(open)  # Funkcija koja pretvara listu u heap queue
+    closed = set()  # Closed je lista zatvorenih stanja koja se sastoji od naziva stanja, ako bi ju punili čvorovima, onda bi razlikovalo ista stanja kao više različitih zbog parenta, implementirana je kao set() zbog brzinske prednosti nad listom
+    total_cost = 0
+
+    heapq.heappush(open, (0, start_state))
+    while len(open) != 0:
+        # Uzimamo prvo stanje iz liste otvorenih koje postaje trenutno stanje
+        price, current_state = heapq.heappop(open)
+        # Ako taj čvor nije u listi zatvorenih, dodajemo ga i obrađujemo, inaće zbog brzine preskačemo 
+        if current_state.name not in closed:
+            closed.add(current_state.name)
+
+            # Provjeravamo je li trenutno stanje konačno stanje
+            if current_state.name in end_state:
+                path = []
+                while current_state.name != start_state.name:
+                    path.append(current_state.name)
+                    temp = current_state.name  # Spremamo ime trenutnog stanja
+                    current_state = current_state.parent  # Dohvaćamo parenta trenutnog stanja
+                    total_cost += successors[current_state.name][temp]  # Zbrajamo trošak putavanja iz parenta u trenutno stanje
+                path.append(start_state.name)
+
+                # Vraćamo potvrdu uspješnosti, broj prođenih stanja, duljinu putanje, totalnu cijenu putanje te putanju 
+                return "yes", len(closed), len(path), float(total_cost), " => ".join(list(reversed(path)))
+
+            for state in successors[current_state.name].keys():
+                state_value, state_key = None, None
+                for (value, key) in open:
+                    #print(f"{value} - {key.name}")
+                    if (key.name == state):
+                        state_value, state_key = value, key
+                        if ((price + (successors[current_state.name])[state]) < state_value - heuristics[key.name]):
+                            open.remove((state_value, state_key))
+                            heapq.heapify(open)
+                heapq.heappush(open, (price + (successors[current_state.name])[state] + heuristics[current_state.name], Node(state, current_state)))
 
     # Ukoliko je pretraživanje bilo neuspješno vraćamo potvrdu neuspješnosti
     return "no", None, None, None, None
@@ -145,47 +195,47 @@ def main():
             consistent = sys.argv[i+1]
 
     if algorithm == "bfs":
-       print("# BFS")
+        print("# BFS")
 
-       start_state, end_state, transitions = get_data(path_state)
+        start_state, end_state, transitions = get_data(path_state)
 
-       found_solution, states_visited, path_length, total_cost, path = bfs(start_state, end_state, transitions)
+        found_solution, states_visited, path_length, total_cost, path = bfs(start_state, end_state, transitions)
 
-       print(f"[FOUND_SOLUTION]: {found_solution}")
-       if found_solution:
-           print(f"[STATES_VISITED]: {states_visited}")
-           print(f"[PATH_LENGTH]: {path_length}")
-           print(f"[TOTAL_COST]: {total_cost}")
-           print(f"[PATH]: {path}")
+        print(f"[FOUND_SOLUTION]: {found_solution}")
+        if found_solution:
+            print(f"[STATES_VISITED]: {states_visited}")
+            print(f"[PATH_LENGTH]: {path_length}")
+            print(f"[TOTAL_COST]: {total_cost}")
+            print(f"[PATH]: {path}")
 
     elif algorithm == "ucs":
-       print("# UCS")
+        print("# UCS")
 
-       start_state, end_state, successors = get_data(path_state)
+        start_state, end_state, successors = get_data(path_state)
 
-       found_solution, states_visited, path_length, total_cost, path = ucs(start_state, end_state, successors)
+        found_solution, states_visited, path_length, total_cost, path = ucs(start_state, end_state, successors)
 
-       print(f"[FOUND_SOLUTION]: {found_solution}")
-       if found_solution:
-           print(f"[STATES_VISITED]: {states_visited}")
-           print(f"[PATH_LENGTH]: {path_length}")
-           print(f"[TOTAL_COST]: {total_cost}")
-           print(f"[PATH]: {path}")
+        print(f"[FOUND_SOLUTION]: {found_solution}")
+        if found_solution:
+            print(f"[STATES_VISITED]: {states_visited}")
+            print(f"[PATH_LENGTH]: {path_length}")
+            print(f"[TOTAL_COST]: {total_cost}")
+            print(f"[PATH]: {path}")
 
     elif algorithm == "astar":
-       print(f"# A-STAR {path_heuristic}")
+        print(f"# A-STAR {path_heuristic}")
 
-       # algorithm
+        start_state, end_state, successors = get_data(path_state)
+        heuristics = get_heuristics_data(path_heuristic)
 
-       start_state, end_state, successors = get_data("istra.txt")
-       heuristics = get_heuristics_data("istra_pessimistic_heuristic.txt")
+        found_solution, states_visited, path_length, total_cost, path = astar(start_state, end_state, successors, heuristics)
 
-       print(f"[FOUND_SOLUTION]: {found_solution}")
-       if found_solution:
-           print(f"[STATES_VISITED]: {states_visited}")
-           print(f"[PATH_LENGTH]: {path_length}")
-           print(f"[TOTAL_COST]: {total_cost}")
-           print(f"[PATH]: {path}")
+        print(f"[FOUND_SOLUTION]: {found_solution}")
+        if found_solution:
+            print(f"[STATES_VISITED]: {states_visited}")
+            print(f"[PATH_LENGTH]: {path_length}")
+            print(f"[TOTAL_COST]: {total_cost}")
+            print(f"[PATH]: {path}")
 
 
 
@@ -204,7 +254,7 @@ if __name__ == "__main__":
 # POKRENEMO TESTNI PRIMJER
 # python solution.py --alg astar --ss istra.txt --h istra_heuristic.txt
 
-# TODO: dodati implementaciju više end_statea
-# TODO: dodati implementaciju nepostojanja valuea za neke transitionse
-# TODO: prepraviti kod da nije state based (možda)
-# TODO: vidjeti vremenski prekid
+# IZVORI
+# https://www.geeksforgeeks.org/python-linked-list/ za Node()
+# https://www.geeksforgeeks.org/deque-in-python/ za deque()
+# https://www.geeksforgeeks.org/heap-queue-or-heapq-in-python/ za heapq()

ac80e91e716f0ac3517fefd935da238de64c916a
MateoJaksic <mateo.jaksic@outlook.com>
2024-04-10 01:00:36 +0200

    Prolazi 25/26 testova

--- autograder/solutions/0036535152/lab2py/solution.py
+++ autograder/solutions/0036535152/lab2py/solution.py
@@ -8,7 +8,7 @@ def get_clauses(path_to_clauses):
     with open(path_to_clauses, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
-    clauses = [line.rstrip().lower() for line in lines if line[0] != "#"]
+    clauses = [(i+1, lines[i].rstrip().lower().split(" v "), (None, None)) for i in range(len(lines)) if lines[i][0] != "#"]
     return clauses
 
 
@@ -22,27 +22,30 @@ def get_commands(path_to_user_commands):
     return commands
 
 
-# Funkcija za negiranje literala
-def negate(clause):
-    if type(clause) == list:
-        if len(clause) == 1:
-            if clause[0] == "~":
-                return clause[1:]
-            else:
-                return ["~" + str(clause[0])]
-        if len(clause) > 1:
-            negated_clause = []
-            for literal in clause:
-                if literal[0] == "~":
-                    negated_clause.append(literal[1:])
-                else:
-                    negated_clause.append(["~" + str(literal[0])])
-            return negated_clause
-    elif type(clause) == str:
-        if clause[0] == "~":
-            return clause[1:]
+# Funkcija za negiranje klauzule
+def negate_clause(clause):
+    print(clause)
+    if len(clause) == 1:
+        if clause[0][0] == "~":
+            return clause[0][1:]
         else:
-            return "~" + clause
+            return [["~" + str(clause[0])]]
+    else:
+        negated_clause = []
+        for literal in clause:
+            if literal[0] == "~":
+                negated_clause.append(literal[1:])
+            else:
+                negated_clause.append(["~" + str(literal)])
+        return negated_clause
+
+
+# Funkcija za negiranje literala
+def negate_literal(literal):
+    if literal[0] == "~":
+        return literal[1:]
+    else:
+        return "~" + literal
 
 
 # Funkcija za uklanjanje redundantnih klauzula
@@ -66,125 +69,131 @@ def coverage_check(clauses):
 
 # Funkcija za uklanjanja klauzula koje su tautologija
 def tautology_check(clause):
-    for first_literal in clause:
-        for second_literal in clause:
-            if clause.index(first_literal) != clause.index(second_literal):
-                if negate(first_literal) in second_literal:
-                    return True
+    for literal in clause:
+        if negate_literal(literal) in clause:
+            return True
 
     return False
 
 
+def factorization(clause):
+    factorized_clause = []
+    for literal in clause: 
+        if literal not in factorized_clause:
+            factorized_clause.append(literal)
+
+    return factorized_clause
+
+# Funkcija za izgradnju klauzule
+def clause_builder(first_clause, second_clause, literal, index_last):
+    clause = [curr_literal for curr_literal in chain(first_clause[1], second_clause[1]) if (curr_literal != literal and curr_literal != negate_literal(literal))]
+
+    return (index_last, factorization(clause), (second_clause[0], first_clause[0]))
+
+
 # Funkcija za prvi podzadatak
 def resolution(clauses):
-    working_clauses = [(i+1, clauses[i].split(" v ")) for i in range(len(clauses)-1)]
-    # Provjera tautologije za postojeće klauzule
-    working_clauses = [None if tautology_check(working_clause[1]) else working_clause for working_clause in working_clauses]
-    working_clauses = coverage_check(working_clauses)
-
-    counter = len(working_clauses) 
-    sos_clauses = []
-    if len(clauses[-1].split(' v ')) == 1:
-        [sos_clauses.append((len(working_clauses) + 1, negate(clauses[-1].split(" v "))))]
-    else:
-        for literal in negate(clauses[-1].split(" v ")):
+    finish_index = len(clauses) - 1
+    counter = len(clauses) - 1
+    for clause in negate_clause(clauses[-1][1]):
+        if counter == finish_index:
+            clauses[counter] = (clauses[counter][0], clause, (None, None))
             counter += 1
-            sos_clauses.append((counter, literal))
-            
+        else:
+            print(clause)
+            clauses.append((clauses[-1][0] + 1, clause, (None, None)))
+            counter += 1
     expanded_clauses = []
-    for first_clause in sos_clauses:
-        for literal in first_clause[1]:
-            for second_clause in working_clauses:
-                if second_clause != None:
-                    if negate(literal) in second_clause[1]:
-                        expanded_clauses.append((second_clause[0],  
-                                                " v ".join(second_clause[1]),
-                        ))
-                        if len(first_clause) == 3:
-                            expanded_clauses.append((first_clause[0],     
-                                                    " v ".join(first_clause[1]),
-                                                    first_clause[2]
-                            ))
-                        else:
-                            expanded_clauses.append((first_clause[0],    
-                                                    " v ".join(first_clause[1]),
-                            ))
+    used_clauses_pairs = []
 
-                        sos_clauses.append((sos_clauses[-1][0] + 1,
-                                            [curr_literal for curr_literal in chain(first_clause[1], second_clause[1]) if (curr_literal != literal and curr_literal != negate(literal))],
-                                            (second_clause[0], first_clause[0])
-                        ))
+    print(clauses)
 
-                        working_clauses[working_clauses.index(second_clause)] = None
+    for clause in clauses:
+        print(clause)
+        if tautology_check(clause[1]):
+            clauses[clauses.index(clause)] = None
+    clauses = coverage_check(clauses)
 
-                        if sos_clauses[-1][1] == []:
-                            expanded_clauses.append((sos_clauses[-1][0],
-                                                    "NIL",
-                                                    sos_clauses[-1][2]
+    print(clauses)
 
-                            ))
-                            return True, expanded_clauses
+    found = False
+    index = finish_index
+    while index < len(clauses):
+        first_clause = clauses[index]
+        found = False
+        print(f"FIRST CLAUSE FOR PETLJA {first_clause}")
+        if first_clause != None:
+            for literal in first_clause[1]:
+                print(f"   LITERAL FOR PETLJA {literal}")
+                if found == False:
+                    for second_clause in clauses:
+                        print(f"      SECOND CLAUSE FOR PETLJA {second_clause}, A FIRST CLAUSE JE {first_clause}")
+                        if second_clause != None and (first_clause[0], second_clause[0]) not in used_clauses_pairs:
+                            if negate_literal(literal) in second_clause[1]:
+                                print(f"         PRONAĐENI KOMPLEMENTARNI LITERALI IF ZA {first_clause} - {second_clause} - {literal}")
+                                found = True
+                                if first_clause not in expanded_clauses:
+                                    expanded_clauses.append(first_clause)
+                                if second_clause not in expanded_clauses:
+                                    expanded_clauses.append(second_clause)
+                                expanded_clauses.append(clause_builder(first_clause, second_clause, literal, len(clauses) + 1))
+                                clauses.append(clause_builder(first_clause, second_clause, literal, len(clauses) + 1))
+                                clauses = coverage_check(clauses)
+                                print(f"            NOVA KLAUZULA JE {clauses[-1]}")
+                                used_clauses_pairs.append((first_clause[0], second_clause[0]))
 
-    if len(expanded_clauses) == 0:
-        for clause in working_clauses:
-            if clause != None:  
-                expanded_clauses.append(clause)
-        for clause in sos_clauses:
-            expanded_clauses.append(clause)
-    
-    # # Dodavanje postojećih klauzula u listu za ispis
-    # working_clauses = [(i+1, clauses[i].split(" v ")) for i in range(len(clauses))]
-    # expanded_clauses = [(i+1, clauses[i]) for i in range(len(clauses))]
- 
-    # # Prepravljanje ciljne klauzule u negiranu vrijednost
-    # working_clauses[-1] = (working_clauses[-1][0], negate(working_clauses[-1][1]))
-    # expanded_clauses[-1] = (expanded_clauses[-1][0], negate(expanded_clauses[-1][1]))
+                                if expanded_clauses[-1][1] == []:
+                                    expanded_clauses[-1] = (expanded_clauses[-1][0], ["NIL"], expanded_clauses[-1][2])
+                                    return True, expanded_clauses
 
-    # working_clauses = [None if tautology_check(working_clause[1]) else working_clause for working_clause in working_clauses]
+                                if tautology_check(expanded_clauses[-1][1]):
+                                    return False, []
 
-    # working_clauses = coverage_check(working_clauses)
-    # for first_clause in working_clauses:
-    #     found = False
-    #     if first_clause != None:
-    #         #print(f"Istražujemo {first_clause[0]}. klauzulu {first_clause[1]}")
-    #         for literal in first_clause[1]:
-    #             #print(f"   Provjeravamo za literal {literal}")
-    #             for second_clause in working_clauses:
-    #                 if second_clause != None:
-    #                     #print(f"      Provjeravamo u {second_clause[0]}. klauzuli {second_clause[1]}")
-    #                     if working_clauses.index(first_clause) != working_clauses.index(second_clause):
-    #                         #print(f"         Gledamo postoji li literal {negate(literal)} u klauzuli")
-    #                         if negate(literal) in second_clause[1]:
-    #                             #print(f"            Iz {first_clause[0]}. klauzule {first_clause[1]} uzimamo literal {literal} i pronalazimo komplementarni literal u {second_clause[0]}. klauzuli {second_clause[1]}")
-    #                             if tautology_check([curr_literal for curr_literal in chain(first_clause[1], second_clause[1]) if (curr_literal != literal and curr_literal != negate(literal))]) == False:
-    #                                 expanded_clauses.append((len(expanded_clauses) + 1, 
-    #                                                         " v ".join([curr_literal for curr_literal in chain(first_clause[1], second_clause[1]) if (curr_literal != literal and curr_literal != negate(literal))]),
-    #                                                         (working_clauses.index(first_clause) + 1, working_clauses.index(second_clause) + 1)))
-    #                                 working_clauses.append((len(working_clauses) + 1,
-    #                                                         [curr_literal for curr_literal in chain(first_clause[1], second_clause[1]) if (curr_literal != literal and curr_literal != negate(literal))]
-    #                                 ))
-    #                                 if working_clauses[-1][1] == []:
-    #                                     expanded_clauses.append((expanded_clauses[-1][0], 
-    #                                                             "NIL",
-    #                                                             expanded_clauses[-1][2]))
-    #                                     expanded_clauses.pop(-2)
-    #                                     successful = True
-    #                                     return successful, expanded_clauses
-    #                                 working_clauses[working_clauses.index(first_clause)] = None
-    #                                 working_clauses[working_clauses.index(second_clause)] = None
-    #                                 found = True
-    #                                 #print(f"            Expanded clauses je {expanded_clauses}")
-    #                                 #print(f"            Working clauses je {working_clauses}")
-    #                                 break
-    #                     if found:
-    #                         break
-    #             if found:
-    #                 break
+        index += 1
 
-    #print(f"Expanded clauses je {expanded_clauses}")
-    #print(f"Working clauses je {working_clauses}")
+    if len(expanded_clauses) > 0:
+        if expanded_clauses[-1][1] != ["NIL"]:
+            found = False
+            expanded_clauses = []
+            used_clauses_pairs = []
+            clauses = clauses[:finish_index + 1]
+            index = finish_index
+            while index < len(clauses):
+                first_clause = clauses[index]
+                found = False
+                print(f"FIRST CLAUSE FOR PETLJA {first_clause}")
+                if first_clause != None:
+                    for literal in reversed(first_clause[1]):
+                        print(f"   LITERAL FOR PETLJA {literal}")
+                        if found == False:
+                            for second_clause in clauses:
+                                print(f"      SECOND CLAUSE FOR PETLJA {second_clause}, A FIRST CLAUSE JE {first_clause}")
+                                if second_clause != None and (first_clause[0], second_clause[0]) not in used_clauses_pairs:
+                                    if negate_literal(literal) in second_clause[1]:
+                                        print(f"         PRONAĐENI KOMPLEMENTARNI LITERALI IF ZA {first_clause} - {second_clause} - {literal}")
+                                        found = True
+                                        if first_clause not in expanded_clauses:
+                                            expanded_clauses.append(first_clause)
+                                        if second_clause not in expanded_clauses:
+                                            expanded_clauses.append(second_clause)
+                                        expanded_clauses.append(clause_builder(first_clause, second_clause, literal, clauses[-1][0]))
+                                        clauses.append(clause_builder(first_clause, second_clause, literal, clauses[-1][0]))
+                                        clauses = coverage_check(clauses)
+                                        print(f"         NOVA KLAUZULA JE {clauses[-1]}")
+                                        used_clauses_pairs.append((first_clause[0], second_clause[0]))
+
+                                        if expanded_clauses[-1][1] == []:
+                                            expanded_clauses[-1] = (expanded_clauses[-1][0], ["NIL"], expanded_clauses[-1][2])
+                                            return True, expanded_clauses
+
+                                        if tautology_check(expanded_clauses[-1][1]):
+                                            return False, []
+
+                index += 1
 
     return False, expanded_clauses
+
+
 
 # Glavna funkcija 
 def main():
@@ -196,16 +205,18 @@ def main():
 
         if flag == "resolution":
             clauses = get_clauses(path_to_clauses)
+            finish_index = len(clauses) - 1
+            finish_clause = clauses[finish_index]
             successful, expanded_clauses = resolution(clauses)
 
-            [print(f"{expanded_clause[0]}. {expanded_clause[1]}") for expanded_clause in sorted(expanded_clauses) if len(expanded_clause) == 2]
+            [print(f"{expanded_clause[0]}. {' v '.join(expanded_clause[1])}") for expanded_clause in sorted(expanded_clauses) if expanded_clause[2] == (None, None)]
             print("===============")
-            [print(f"{expanded_clause[0]}. {expanded_clause[1]} ({expanded_clause[2][0]}, {expanded_clause[2][1]})") for expanded_clause in sorted(expanded_clauses) if len(expanded_clause) == 3]
+            [print(f"{expanded_clause[0]}. {' v '.join(expanded_clause[1])} ({expanded_clause[2][0]}, {expanded_clause[2][1]})") for expanded_clause in sorted(expanded_clauses) if expanded_clause[2] != (None, None)]
             print("===============")
             if successful == True:
-                print(f"[CONCLUSION]: {clauses[-1]} is true")
+                print(f"[CONCLUSION]: {' v '.join(finish_clause[1])} is true")
             else:
-                print(f"[CONCLUSION]: {clauses[-1]} is unknown")
+                print(f"[CONCLUSION]: {' v '.join(finish_clause[1])} is unknown")
 
 
     elif (len(sys.argv) == 4):
@@ -216,8 +227,6 @@ def main():
         if flag == "cooking":
             clauses = get_clauses(path_to_clauses)
             commands = get_commands(path_to_user_commands)
-
-
 
 
 if __name__ == "__main__":
